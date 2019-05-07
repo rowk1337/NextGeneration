63be1f2f40798b0cdc3a02e2e36b05f85ac28d33
rowk1337 <44575317+rowk1337@users.noreply.github.com>
2019-05-07 23:03:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -173,9 +173,9 @@ async def on_message(message):
                                           "rowkzao | RedcepP#8728\n\n"
                                           "__***Administradores:***__\n"
                                           "KonigaN#5948 | leandroflorencio#5141 | rowk1337#8223 | \n\n"
-        embed.set_thumbnail(url = server.icon_url);
+        embed.set_thumbnail(url=message.server.icon_url)
         embed.set_footer(
-            icon_url="https://cdn.discordapp.com/avatars/385029583696691211/2b465b31ca58fb4cb9b853a2ab6b9406.png?size=128",
+            icon_url="https://cdn.discordapp.com/attachments/575010844161867777/575418704125231131/Lusitanologo.png",
             text="rowk1337#8223")
         await client.send_message(message.channel, embed=embed)
 
